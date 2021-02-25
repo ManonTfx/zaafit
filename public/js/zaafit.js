e14af368c0d76b7b8f68040e06ed9f9f913c48fe
ManonTfx <manon.trefoux@gmail.com>
2021-02-25 14:01:40 +0100

    User entity and Security

---
+++ public/js/zaafit.js
@@ -0,0 +1,21 @@
+//menu Burger
+
+// const menuBurger = document.querySelector('.buttonMenuBurger');
+// const menuCache = document.querySelector('.menucache');
+// const displayButton = document.querySelector('.displaybutton');
+
+// function addOrRemoveClass(element, classElement){
+//     const listOfClass = element.className.split(' ')
+//     let hasClass = 0
+//     listOfClass.forEach(c => {
+//         if (c === classElement) hasClass = 1
+//     });
+//     if (hasClass === 1) element.classList.remove(classElement)
+//     else element.classList.add(classElement)
+// };
+
+// menuBurger.onclick = function(){
+//     addOrRemoveClass(this,'marginButtonMenuBurger');
+//     addOrRemoveClass(displayButton, 'displayButton');
+//     addOrRemoveClass(menuCache, 'displayMenuCache');
+// };
