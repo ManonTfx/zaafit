bfd035e89509f92557afa7378bcb0f9054e4d74c
ManonTfx <manon.trefoux@gmail.com>
2021-02-25 15:25:36 +0100

    register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,20 +19,38 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
+            ->add('nom', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Nom',
+                )
+            ))
+            ->add('prenom', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Prénom',
+                )
+            ))
+            ->add('email', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Adresse mail',
+                )
+            ))
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les conditions générales',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes ne sont pas identiques',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['attr' => ['placeholder' => 'Mot de passe', 'label' => 'Password']],
+                'second_options' => ['attr' => ['placeholder' => 'Confirmation du passe', 'label' => 'Repeat Password']],
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
