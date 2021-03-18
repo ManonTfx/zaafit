d120c8df41d73d4343240b8cf4fa3eb272a5de5b
ManonTfx <manon.trefoux@gmail.com>
2021-03-18 22:49:42 +0100

    Add registration Form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,10 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Historique;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -27,6 +32,28 @@ class RegistrationFormType extends AbstractType
             ->add('prenom', TextType::class, array(
                 'attr' => array(
                     'placeholder' => 'Prénom',
+                )
+            ))
+            ->add('sexe', ChoiceType::class, [
+                'choices'  => [
+                    'Femme' => 'f',
+                    'Homme' => 'h',
+                ]
+            ])
+            ->add('imageFile', FileType::class)
+            ->add('taille_user', NumberType::class, array(
+                'attr' => array(
+                    'placeholder' => '000',
+                )
+            ))
+            ->add('objectif_poids', NumberType::class, array(
+                'attr' => array(
+                    'placeholder' => '00.0',
+                )
+            ))
+            ->add('poids_depart', NumberType::class, array(
+                'attr' => array(
+                    'placeholder' => '00.0',
                 )
             ))
             ->add('email', TextType::class, array(
