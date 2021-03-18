25078610e1172dfe3a9c610d97bd18d5cecb1aac
ManonTfx <manon.trefoux@gmail.com>
2021-03-18 22:36:28 +0100

    Add Entity Poids User

---
+++ src/Entity/Poids.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PoidsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PoidsRepository::class)
+ */
+class Poids
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="decimal", precision=5, scale=1)
+     */
+    private $user_poids;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="poids_user")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_jour;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserPoids(): ?string
+    {
+        return $this->user_poids;
+    }
+
+    public function setUserPoids(string $user_poids): self
+    {
+        $this->user_poids = $user_poids;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getDateJour(): ?\DateTimeInterface
+    {
+        return $this->date_jour;
+    }
+
+    public function setDateJour(\DateTimeInterface $date_jour): self
+    {
+        $this->date_jour = $date_jour;
+
+        return $this;
+    }
+}
