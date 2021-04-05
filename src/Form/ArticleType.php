aa4ec5fffeaede6781df90c58b8c5200ddbe8759
ManonTfx <manon.trefoux@gmail.com>
2021-04-05 19:42:17 +0200

    Add Form create or update article

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Categorie;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('contenu', CKEditorType::class)
+            ->add('imageFile', filetype::class)
+            ->add('categories', EntityType::class, [
+                'class' => Categorie::class,
+                'multiple'   => true,
+                'expanded' => true,
+                'choice_label' => 'nom',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
