e887defab823bcadcd59293bcc18c36f40582e15
ManonTfx <manon.trefoux@gmail.com>
2021-04-06 21:19:15 +0200

    Function AddOrRemove() for menuburger

--- public/js/zaafit.js
+++ public/js/zaafit.js
@@ -1,24 +1,25 @@
-//menu Burger
+//menu Burger Repas
 
 const menuBurgerBtn = document.querySelector('.menuburgerbtn');
-const menuCache = document.querySelector('.navmenuburger');
+const menuCache = document.querySelector('.navmenuburgerfoodandsport');
+const masqueMenu = document.querySelector('.masquemenu');
+const afficheMenu = document.querySelector('.affichemenu');
+const containerHomeUser = document.querySelector('.containerFoodAndSportUser');
 
 
-// const displayButton = document.querySelector('.displaybutton');
+function addOrRemoveClass(element, classElement){
+    const listOfClass = element.className.split(' ')
+    let hasClass = 0
+    listOfClass.forEach(c => {
+        if (c === classElement) hasClass = 1
+    });
+    if (hasClass === 1) element.classList.remove(classElement)
+    else element.classList.add(classElement)
+};
 
-// function addOrRemoveClass(element, classElement){
-//     const listOfClass = element.className.split(' ')
-//     let hasClass = 0
-//     listOfClass.forEach(c => {
-//         if (c === classElement) hasClass = 1
-//     });
-//     if (hasClass === 1) element.classList.remove(classElement)
-//     else element.classList.add(classElement)
-// };
-
-// menuBurger.onclick = function(){
-//     addOrRemoveClass(this,'marginButtonMenuBurger');
-//     addOrRemoveClass(displayButton, 'displayButton');
-//     addOrRemoveClass(menuCache, 'displayMenuCache');
-// };
-
+menuBurgerBtn.onclick = function(){
+    addOrRemoveClass(masqueMenu,'displayNone');
+    addOrRemoveClass(menuCache, 'displayNone');
+    addOrRemoveClass(containerHomeUser, 'marginleft200');
+    addOrRemoveClass(afficheMenu, 'displayBlock');
+};
